a164e328c5e9c5d3e0f51fb7fa3e98301140e220
pedrohrs <pedrohenriqueinf@hotmail.com>
2014-05-07 19:35:42 -0300

    Adiciona get e set

--- sistema/classes/Oferta.php
+++ sistema/classes/Oferta.php
@@ -3,4 +3,30 @@ class Oferta{
   private $usuario;
   private $item;
   private $descricao;
+  
+  public function getUsuario() {
+      return $this->usuario;
+  }
+
+  public function getItem() {
+      return $this->item;
+  }
+
+  public function getDescricao() {
+      return $this->descricao;
+  }
+
+  public function setUsuario($usuario) {
+      $this->usuario = $usuario;
+  }
+
+  public function setItem($item) {
+      $this->item = $item;
+  }
+
+  public function setDescricao($descricao) {
+      $this->descricao = $descricao;
+  }
+
+
 }
