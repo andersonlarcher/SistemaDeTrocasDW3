ebfef7f0de4320d4720ca260a600747736332aff
Anderson <andersonfranco19@gmail.com>
2014-05-13 11:01:56 -0300

    Criado Get e Set da classe Proposta

--- sistema/classes/Proposta.php
+++ sistema/classes/Proposta.php
@@ -15,4 +15,30 @@ class Proposta {
         $this->Usuario = $usuario;
         $this->Oferta = $oferta;
     }
+    
+    public function getUsuario() {
+        return $this->usuario;
+    }
+
+    public function getOferta() {
+        return $this->oferta;
+    }
+
+    public function getDesejo() {
+        return $this->desejo;
+    }
+
+    public function setUsuario($usuario) {
+        $this->usuario = $usuario;
+    }
+
+    public function setOferta($oferta) {
+        $this->oferta = $oferta;
+    }
+
+    public function setDesejo($desejo) {
+        $this->desejo = $desejo;
+    }
+
+
 }
