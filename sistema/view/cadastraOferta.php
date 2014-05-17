e010b53297ba5502ab03c9d77cf313006bbf2a35
pedrohrs <pedrohenriqueinf@hotmail.com>
2014-05-17 17:47:57 -0300

    view para cadastrar oferta

---
+++ sistema/view/cadastraOferta.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cadasta Oferta</title>
+        <link rel="stylesheet" href="../../css/cad-oferta.css" type="text/css">
+    </head>
+    <body>
+        <h1>Cadastra Oferta</h1>
+        <form action="" method="post">
+            <label>Nome</label>
+            <input type="text" name="usuario">
+            <label>Item</label>
+            <input type="text" name="item">
+            <label>Descrição</label>
+            <textarea name="descricao" rows="5" cols="20"></textarea>
+            <button type="submit" name="cad_oferta">Cadastrar</button>
+        </form>
+    </body>
+</html>
