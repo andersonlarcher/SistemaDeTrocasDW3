393fae6e3740a0559655d8c42f54b4fa0fd9c09d
pedrohrs <pedrohenriqueinf@hotmail.com>
2014-05-17 17:47:19 -0300

    Criação do index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $view = "./sistema/view/";
+;?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="<?= $view ?>cadastraOferta.php">Cadastro Oferta</a>
+        <?php
+            
+        ?>
+    </body>
+</html>
