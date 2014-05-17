f6c0ffcc9bf5b60618fc627791fb9fbfe8e30887
pedrohrs <pedrohenriqueinf@hotmail.com>
2014-05-17 17:46:46 -0300

    Acrescentando atributos e get e set

--- sistema/classes/Usuario.php
+++ sistema/classes/Usuario.php
@@ -1,16 +1,47 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Usuario
  *
  * @author Anderson
  */
 class Usuario {
-    //put your code here
+    private $cod;
+    private $nome;
+    private $email;
+    private $tel;
+    
+    public function getCod() {
+        return $this->cod;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getTel() {
+        return $this->tel;
+    }
+
+    public function setCod($cod) {
+        $this->cod = $cod;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setTel($tel) {
+        $this->tel = $tel;
+    }
+
+
 }
